e39354755f48c0ee1a89a86e84dd70606b1302ac
smahli <41840920+smahli@users.noreply.github.com>
2020-11-09 13:29:26 +0530

    Add files via upload

---
+++ OpPrecedence.js
@@ -0,0 +1,23 @@
+/*
+operator precedence  concepts
+ */
+var x,y,z;
+x=2;
+y=4;
+z= (x+4*45)/7+y;
+console.log(z);
+
+
+
+// some more operator
+x=x+2;
+console.log(x);
+x+=2;
+console.log(x);
+
+
+// increment operator
+y--;
+x++;
+console.log(y);
+console.log(x);
