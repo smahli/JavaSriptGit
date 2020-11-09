5bda69d58d5e03d2a99ced9639277562310a4146
smahli <41840920+smahli@users.noreply.github.com>
2020-11-09 13:28:11 +0530

    Add files via upload

---
+++ BoolLogic.js
@@ -0,0 +1,19 @@
+/** Boolean logic 
+ * we will talk about AND OR and NOT Logic 
+ */
+var Name= 'John';
+var Age=11;
+if(Age>=0 && Age <13){
+	console.log(Name,' is a child');
+}
+
+else if(Age >= 13  && Age <20){
+	console.log(Name , ' Is a teenager');
+}
+else if( Age >= 20 && Age <30){
+	console.log(Name, ' is a young ');
+}else{
+
+	console.log(Name, ' Is a man ');
+
+}
