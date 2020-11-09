1edd796ca1dcd9b376e54a3acbde056ec7d14dfa
smahli <41840920+smahli@users.noreply.github.com>
2020-11-09 13:27:07 +0530

    Add files via upload

---
+++ BasicOperators.js
@@ -0,0 +1,32 @@
+ //Here we gonna learn Basic opeartors
+
+ // Maths operators
+ var now =2020;
+ var MyAge, MyFriendAge, MyYear, MyFriendYear;
+ MyAge=24;
+ MyFriendAge=28;
+
+ MyYear= now-MyAge;
+ MyFriendYear= now-MyFriendAge;
+ console.log(MyYear,MyFriendYear);
+ // basic maths operator
+ console.log(now + 2);
+ console.log(now *2);
+ console.log(now-200);
+ console.log(now/10);
+
+
+
+ // Logical oprators
+
+var IsOlder;
+IsOlder= MyAge>MyFriendAge;
+console.log(IsOlder);
+
+// typeofoprator
+console.log(typeof IsOlder);
+console.log(typeof now);
+
+
+ 
+
